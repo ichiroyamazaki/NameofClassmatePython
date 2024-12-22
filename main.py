ee11cfbddf18b0f8a683151f702c0b2471915431
Ichiro Yamazaki <16699009+ichiroyamazaki@users.noreply.github.com>
2024-12-22 23:42:35 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+name1 = input("Enter the name of the first classmate: ")
+name2 = input("Enter the name of the second classmate: ")
+name3 = input("Enter the name of the third classmate: ")
+
+print("The names of your three classmates are:")
+print (name1)
+print (name2) 
+print (name3)
